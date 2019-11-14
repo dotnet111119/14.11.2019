23d8653e949a1ca55c36e03c9ff994f49780f105
dotnet111119 <57640157+dotnet111119@users.noreply.github.com>
2019-11-14 21:05:55 +0200

    Add files via upload

---
+++ Targil_sol/1411/Car.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1411
+{
+    public class Car
+    {
+        #region DATA
+
+        public string _model;
+        public int _year;
+        public string _color;
+        public float _height;
+        public bool _work;
+
+        #endregion
+
+        #region Methods
+
+        public Car()
+        {
+
+        }
+
+        public Car(string _model)
+        {
+            this._model = _model;
+        }
+
+        public Car(string _model, int _year, string _color, float _height, bool _work)
+        {
+            this._model = _model;
+            this._year = _year;
+            this._color = _color;
+            this._height = _height;
+            this._work = _work;
+        }
+
+        public void FixCar()
+        {
+            _work = true;
+        }
+
+        public void Paint(string color)
+        {
+            _color = color;
+        }
+
+        public void BadTire()
+        {
+            Console.WriteLine("Oh no....");
+            _height = _height / 2.0f;
+            _work = false;
+        }
+
+        #endregion
+    }
+}
---
+++ Targil_sol/1411/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1411
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car honda = new Car()
+            {
+                _color = "black",
+                _height = 0.7f,
+                _model = "CV",
+                _work = true,
+                _year = 2020
+            };
+            Console.WriteLine(honda);
+            honda.BadTire();
+            Console.WriteLine(honda);
+
+            Car mitsubishi = new Car("Mitsubishi");
+            Console.WriteLine(mitsubishi);
+
+            Car ferrari = new Car("Ferrari", 2021, "red", 0.5f, true);
+            Console.WriteLine(ferrari);
+
+        }
+    }
+}
